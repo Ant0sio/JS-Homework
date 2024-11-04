acab0136aab8c1164363da7e4364134fd70f5343
Ant0sio <antonzeninec@gmail.com>
2024-11-04 18:22:06 +0200

    Homework 1 JS

---
+++ Homework_JS_1-2/main2.js
@@ -0,0 +1,10 @@
+let firstName='Зенинець';
+console.log(firstName)
+let middleName = 'Антон';
+console.log(middleName)
+let lastName = 'Романович';
+console.log(lastName)
+let person = firstName+' '+middleName+' '+lastName
+console.log(person)
+let person2 = `${firstName} ${middleName} ${lastName}`
+console.log(person2)
---
+++ Homework_JS_1-4/main4.js
@@ -0,0 +1,7 @@
+let name=prompt("Яке ваше Ім'я?");
+let middlename=prompt("Як ви по-батькові?");
+let years=prompt("Скільки вам років?");
+
+console.log(name);
+console.log(middlename);
+console.log(years);
