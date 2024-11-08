15be62cf000e99f378a3940e9795a638d67fb910
Ant0sio <antonzeninec@gmail.com>
2024-11-08 14:06:08 +0200

    - add Homework_JS_3

---
+++ Homework_JS_3/Homework_JS_3-3/Homework_JS_3-3.js
@@ -0,0 +1,6 @@
+
+let i = 0;
+while (i < 20) {
+    document.write(`<div><h1>Lorem</h1></div>`);
+    i++;
+}
---
+++ Homework_JS_3/Homework_JS_3-5/Homework_3-5.js
@@ -0,0 +1,11 @@
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write(`<ul>`)
+
+    for (const Item of listOfItems) {
+        document.write(`<li>${Item}</li>`)
+    }
+
+document.write(`</ul>`)
+
+
